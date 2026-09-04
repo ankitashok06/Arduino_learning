f7beb3d339aa5a457057bb63da98da60893343c4
Ankit <ankitaak693@gmail.com>
2026-09-04 22:20:24 +0530

    Update ArrayExample.ino

--- ArrayExample.ino
+++ ArrayExample.ino
@@ -3,24 +3,17 @@
 int sensorData[5] = {100, 110, 120, 130, 10};
 
 void setup() {
-  // Start Serial Monitor communication
   Serial.begin(9600);
 }
 
 void loop() {
   int sum = 0;
-
-  // Add all sensor values
   for (int i = 0; i < 5; i++) {
     sum = sum + sensorData[i];
   }
-
-  // Calculate average
   float average = (float)sum / 5;
-
-  // Display average in Serial Monitor
+  
   Serial.println(average);
-
-  // Wait for 1 second
+  
   delay(1000);
 }
